c18177d9122a6a66eb9c09ed5a0743cbd22aefc8
Tom W Hartung <tomwhartung@gmail.com>
2016-07-12 02:20:09 +0000

    Yikes forgot to update the program heading.

--- nodeschool/learnyounode/09-juggling_async/program.js
+++ nodeschool/learnyounode/09-juggling_async/program.js
@@ -1,14 +1,15 @@
 //
-// ## HTTP COLLECT (Exercise 8 of 13)  
+// ## JUGGLING ASYNC (Exercise 9 of 13)  
 //   
-//  Write a program that performs an HTTP GET request to a URL provided to you  
-//  as the first command-line argument. Collect all data from the server (not  
-//  just the first "data" event) and then write two lines to the console  
-//  (stdout).  
+//  This problem is the same as the previous problem (HTTP COLLECT) in that  
+//  you need to use http.get(). However, this time you will be provided with  
+//  three URLs as the first three command-line arguments.  
 //   
-//  The first line you write should just be an integer representing the number  
-//  of characters received from the server. The second line should contain the  
-//  complete String of characters sent by the server.  
+//  You must collect the complete content provided to you by each of the URLs  
+//  and print it to the console (stdout). You don't need to print out the  
+//  length, just the data as a String; one line per URL. The catch is that you  
+//  must print them out in the same order as the URLs are provided to you as  
+//  command-line arguments.  
 //
 var http = require('http')
 
