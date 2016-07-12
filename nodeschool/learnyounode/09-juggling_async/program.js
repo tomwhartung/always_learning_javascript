132e3300d61c0b6a2e07a914b4325f5c9c79c26e
Tom W Hartung <tomwhartung@gmail.com>
2016-07-11 19:58:30 -0600

    First pass at program.js for the 09-juggling_async challenge.

---
+++ nodeschool/learnyounode/09-juggling_async/program.js
@@ -0,0 +1,50 @@
+//
+// ## HTTP COLLECT (Exercise 8 of 13)  
+//   
+//  Write a program that performs an HTTP GET request to a URL provided to you  
+//  as the first command-line argument. Collect all data from the server (not  
+//  just the first "data" event) and then write two lines to the console  
+//  (stdout).  
+//   
+//  The first line you write should just be an integer representing the number  
+//  of characters received from the server. The second line should contain the  
+//  complete String of characters sent by the server.  
+//
+var http = require('http')
+
+var url1 = process.argv[2];
+var url2 = process.argv[3];
+var url3 = process.argv[4];
+var response = [ '', '', '' ];
+var done = [ false, false, false ];
+
+http.get( url1, getResponse1 );
+http.get( url2, getResponse2 );
+http.get( url3, getResponse3 );
+
+function getResponse1( response ) {
+	processResponse( response, 0 );
+	if( allDone() ) {
+		for( var index = 0; index < response.length; index++ ) {
+			console.log( response[index] );
+		}
+	}
+}
+
+function processResponse( response, index ) {
+	response.setEncoding( "utf8" );
+	response.on( "data", function (data) {
+		response[index] += data;
+	})
+	response.on( "end", function () {
+		done[index] = true;
+	})  
+}
+
+function allDone() {
+	if( done[0] && done[1] && done[2] ) {
+		return true;
+	} else {
+		return false;
+	}
+}
