650c20585efcee7502747b1babe5bcf513fb61b0
Tom W Hartung <tomwhartung@gmail.com>
2016-07-13 14:46:17 -0600

    Got it to work, and left ample comments about mysteries unsolved (for now, anyway) for possible future reference.

--- nodeschool/learnyounode/11-http_file_server/program.js
+++ nodeschool/learnyounode/11-http_file_server/program.js
@@ -19,11 +19,24 @@ var portNum = process.argv[2];
 var fileName = process.argv[3];
 
 var server = http.createServer(function( request, response ) {  
+	console.log( 'creating a readable stream for fileName = ' + fileName + '.' );
 	var readableStream = fs.createReadStream( fileName );
+	var fileData = '';
 	readableStream.on( 'data', (chunk) => {
-	//	readableStream.pipe(response);
-		console.log(`Received ${chunk.length} bytes of data.`);
+		fileData += chunk;
+		console.log( '"on" event: received chunk.length = ' + chunk.length + ' bytes of data.' );
+		response.write( chunk );
 	});
+	// This page:
+	//    https://www.sitepoint.com/basics-node-js-streams/
+	// helped me solve this, as you can see, without using the pipe.
+	// The page at the url above also suggests the code below, for processing end,
+	//    which seems ok but does not work for some reason
+	// Not worrying about that and leaving this program as-is for now though!
+	// readableStream.on( 'end', function() {
+	// //	readableStream.pipe(response);
+	// 	console.log( '"end" event: received a total of fileData.length = ' + fileData.length + ' bytes of data.' );
+	// });
 });
 
 server.listen( portNum );
