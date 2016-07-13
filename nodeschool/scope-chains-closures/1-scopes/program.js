35ae732100c9a72be51ec9ca043882c9cb2614ac
Tom W Hartung <tomwhartung@gmail.com>
2016-07-13 15:39:12 -0600

    Adding the first scope-chains-closures task and solution to this lesson.

---
+++ nodeschool/scope-chains-closures/1-scopes/program.js
@@ -0,0 +1,12 @@
+//
+// # Your Mission
+//
+// In an empty file, create a function foo which contains one variable lexically
+// scoped named bar.
+//
+// Once complete, execute scope-chains-closures verify <your-file.js> to verify your
+// solution.
+
+function foo() {
+	var bar = '';    // this is a lexically scoped variable
+}
