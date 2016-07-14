25ed9794740ed35ae49798cf31183fbb9148585d
Tom W Hartung <tomwhartung@gmail.com>
2016-07-13 21:31:57 -0600

    Program that solves the first functional-javascript challenge.

---
+++ nodeschool/functional-javascript-workshop/01-hello_world/program.js
@@ -0,0 +1,15 @@
+//
+// Hello World
+// Exercise 1 of 18
+//
+// # Task:
+//   Write a function that takes an input string and returns it uppercased.
+// 
+// ## Arguments:
+//  * input: a String of random words (lorem ipsum).
+
+function upperCaser( input ) {
+	return input.toUpperCase();
+}
+    
+module.exports = upperCaser
