2d4392e54fe372cbda89b1d68155f54d68955085
Tom W Hartung <tomwhartung@gmail.com>
2016-07-14 08:44:38 -0600

    Adding the program and task for the second currying challenge, "binary."

---
+++ nodeschool/currying-workshopper/2-binary/program.js
@@ -0,0 +1,12 @@
+//
+// BINARY
+// Exercise 2 of 6
+//
+// Write a function that takes two arguments, returns sum of arguments.
+//
+var binary = function( addend1, addend2 ) {
+	return addend1 + addend2;
+};
+
+module.exports = binary
+
