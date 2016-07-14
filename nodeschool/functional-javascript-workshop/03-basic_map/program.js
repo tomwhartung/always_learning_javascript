800675cebe88919a8b60bd940070835ea58f1f3c
Tom W Hartung <tomwhartung@gmail.com>
2016-07-14 04:12:30 +0000

    Starting point for challenge 03-basic_map .

---
+++ nodeschool/functional-javascript-workshop/03-basic_map/program.js
@@ -0,0 +1,28 @@
+//
+// Basic: Map
+// Exercise 3 of 18
+//
+// # Task
+//
+// Convert the following code from a for-loop to Array#map:
+//
+// function doubleAll(numbers) {
+//    var result = []
+//    for (var i = 0; i < numbers.length; i++) {
+//       result.push(numbers[i] * 2)
+//    }
+//    return result
+// }
+//
+// module.exports = doubleAll
+//
+// ## Arguments
+//
+// * numbers: An Array of 0 to 20 Integers between 0 and 9
+
+function doubleAll(numbers) {
+	// SOLUTION GOES HERE
+}
+
+module.exports = doubleAll
+
