61e1509e8ef35ba60ee8ee6e028fbecf8a2e76cd
Tom W Hartung <tomwhartung@gmail.com>
2016-07-14 08:35:23 -0600

    Adding the program (identity) and task for the first challenge in the currying nodeschool workshop.

---
+++ nodeschool/currying-workshopper/1-identity/program.js
@@ -0,0 +1,12 @@
+//
+// IDENTITY
+// Exercise 1 of 6
+//
+// Write a function that takes an argument, returns that argument.
+//
+var identity = function( args ) {
+	return args;
+};
+
+module.exports = identity
+
