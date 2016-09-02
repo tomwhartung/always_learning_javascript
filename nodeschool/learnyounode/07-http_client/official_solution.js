a843c0f875d31c8e387ff7a5f0a55e5cfe397927
Tom W Hartung <tomwhartung@gmail.com>
2016-09-01 18:34:19 -0600

    Adding the official_solution.js to challenge 07-http_client .

---
+++ nodeschool/learnyounode/07-http_client/official_solution.js
@@ -0,0 +1,10 @@
+/*
+ *   Here's the official solution in case you want to compare notes:  
+ */
+var http = require('http')  
+       
+http.get(process.argv[2], function (response) {  
+   response.setEncoding('utf8')  
+   response.on('data', console.log)  
+   response.on('error', console.error)  
+}).on('error', console.error)  
