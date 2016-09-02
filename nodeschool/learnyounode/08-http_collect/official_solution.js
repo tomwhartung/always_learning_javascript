e8f29e77cc00f6d574a9bd9661a02eb2a8a9ff64
Tom W Hartung <tomwhartung@gmail.com>
2016-09-01 19:15:28 -0600

    Added a couple of comments containing a couple of links found in the Hints.

--- nodeschool/learnyounode/08-http_collect/official_solution.js
+++ nodeschool/learnyounode/08-http_collect/official_solution.js
@@ -4,8 +4,10 @@
  * As suspected, they use an anonymous "lambda" function
  *
  * As unsuspected, they use this "bl" library, which is apparently a buffer list.
- *    https://github.com/rvagg/bl
- *
+ *     https://github.com/rvagg/bl
+ *   Links to docs given in the Hints
+ *     file:///usr/lib/node_modules/learnyounode/docs/bl.html
+ *     file:///usr/lib/node_modules/learnyounode/docs/concat-stream.html
  */
 var http = require('http')  
 var bl = require('bl')  
