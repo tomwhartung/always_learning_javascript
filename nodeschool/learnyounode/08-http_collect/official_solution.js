3fcb25ba00002bbaf54463bcf7e684215f47861f
Tom W Hartung <tomwhartung@gmail.com>
2016-09-01 19:02:43 -0600

    Adding the official_solution.js they give for 08-http_collect .  Apparently I was supposed to use "bl," hmmm.

---
+++ nodeschool/learnyounode/08-http_collect/official_solution.js
@@ -0,0 +1,22 @@
+/*
+ * Here's the official solution in case you want to compare notes:  
+ *
+ * As suspected, they use an anonymous "lambda" function
+ *
+ * As unsuspected, they use this "bl" library, which is apparently a buffer list.
+ *    https://github.com/rvagg/bl
+ *
+ */
+var http = require('http')  
+var bl = require('bl')  
+
+http.get(process.argv[2], function (response) {  
+  response.pipe( bl(function (err, data) {  
+    if (err)  
+      return console.error(err)  
+    data = data.toString()  
+    console.log(data.length)  
+    console.log(data)  
+  }) )    
+})  
+
