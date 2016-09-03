48eaaeb11fe572199a737151a11d34f27dc7b143
Tom W Hartung <tomwhartung@gmail.com>
2016-09-03 15:33:11 -0600

    Updates to complete the first learnyoureact challenge: "hello react."  To get this to work, I simply followed the directions given (and saved in task.txt); for a brief synopsis, see the comments in the header of program.js .

---
+++ nodeschool/learnyoureact/01-hello_react/views/index.jsx
@@ -0,0 +1,15 @@
+/*
+ * This code uses the optional React.js JSX syntax to create our views, which we
+ * shall use throughout the rest of this workshop.
+ */
+
+
+import React from 'react';
+    
+export default class TodoBox extends React.Component{
+   render() {
+      return <div className="todoBox">
+          Hello, world!
+        </div>
+   }
+}
