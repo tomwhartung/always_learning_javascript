89b3c7ec5c5d792b567a0511b387dcd29b9dee82
Tom W Hartung <tomwhartung@gmail.com>
2016-09-04 17:51:48 -0600

    Completed first challenge of the javascripting lesson.

---
+++ nodeschool/javascripting/01-introduction/introduction.js
@@ -0,0 +1,12 @@
+/*
+ * JAVASCRIPTING INTRODUCTION (Exercise 1 of 19)  
+ *
+ * To keep things organized, let's create a folder for this workshop.  
+ *
+ *    mkdir javascripting  
+ *    cd javascripting  
+ *    touch introduction.js  
+ *
+ */
+ 
+console.log('hello');  
