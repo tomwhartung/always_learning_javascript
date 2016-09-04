098fd7954282c7959c2641550b86585ff1ea1296
Tom W Hartung <tomwhartung@gmail.com>
2016-09-04 17:58:38 -0600

    Adding verified solution to the second challenge, variables.

--- nodeschool/javascripting/01-introduction/introduction.js
+++ nodeschool/javascripting/01-introduction/introduction.js
@@ -1,12 +1,12 @@
 /*
- * JAVASCRIPTING INTRODUCTION (Exercise 1 of 19)  
+ * JAVASCRIPTING INTRODUCTION (Exercise 1 of 19)
  *
- * To keep things organized, let's create a folder for this workshop.  
+ * To keep things organized, let's create a folder for this workshop.
  *
- *    mkdir javascripting  
- *    cd javascripting  
- *    touch introduction.js  
+ *    mkdir javascripting
+ *    cd javascripting
+ *    touch introduction.js
  *
  */
- 
-console.log('hello');  
+
+console.log('hello');
