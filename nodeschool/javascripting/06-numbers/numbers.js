cea49a9e879718737f2e64a6d36eac58f8596c5b
Tom W Hartung <tomwhartung@gmail.com>
2016-09-04 18:31:04 -0600

    Adding solution to the sixth challenge: numbers.js .

---
+++ nodeschool/javascripting/06-numbers/numbers.js
@@ -0,0 +1,11 @@
+/*
+ * JAVASCRIPTING - NUMBERS (Exercise 6 of 19)
+ *
+ * The challenge:
+ * Create a file named numbers.js.
+ * In that file define a variable named example that references the integer 123456789.
+ * Use console.log() to print that number to the terminal.
+ */
+
+var example = 123456789;
+console.log( example );
