d1e3ac5d2d4eb23becfdd738740fcb179dd518c0
Tom W Hartung <tomwhartung@gmail.com>
2016-09-04 18:42:49 -0600

    Adding verified solution to challenge 8: number-to-string.js .

---
+++ nodeschool/javascripting/08-number_to_string/number-to-string.js
@@ -0,0 +1,13 @@
+/*
+ * JAVASCRIPTING - NUMBER TO STRING (Exercise 8 of 19)
+ *
+ * The challenge:
+ * Create a file named number-to-string.js.
+ * In that file define a variable named n that references the number 128;
+ * Call the .toString() method on the n variable.
+ * Use console.log() to print the results of the .toString() method to the terminal.
+ */
+
+var n = 128;
+var str = n.toString();
+console.log( str );
