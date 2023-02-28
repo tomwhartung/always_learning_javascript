f4e6226672b5989ecd568a383bf0c2089ec94f1b
Tom W Hartung <tomwhartung@gmail.com>
2023-02-28 13:09:17 -0700

    Updated my-quick-start-app/src/App.js so that BonjourApp is now the "First App" and a new MyStateApp app is now the "Second App."

--- reactjs/projects/my-quick-start-app/src/App.js
+++ reactjs/projects/my-quick-start-app/src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// import logo from './logo.svg';
+
+import './App.css';
+import { useState } from 'react';
+
+//
+// First App: says Hello World in French
+//
+function BonjourApp() {
   return (
     <div className="App">
       <header className="App-header">
@@ -13,4 +19,32 @@ function App() {
   );
 }
 
-export default App;
+//
+// Second App: demonstrates how to use multiple instances of the same state
+//
+function MyStateApp() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+// export default BonjourApp;    // First App above
+export default MyStateApp;    // Second App above
