9980cef0e9b2286a15980e101140370bd780a1e8
Tom W Hartung <tomwhartung@gmail.com>
2023-02-28 18:07:39 -0700

    Added an "AllMyApps" app to reactjs/projects/my-quick-start-app/src/App.js and programmed it to use the two already-existing apps.

--- reactjs/projects/my-quick-start-app/src/App.js
+++ reactjs/projects/my-quick-start-app/src/App.js
@@ -4,27 +4,32 @@
 import './App.css';
 import { useState } from 'react';
 
-//
-// First App: says Hello World in French
-//
-function BonjourApp() {
+// AllMyApps: run all the apps!
+function AllMyApps() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Bonjour, monde!
-        </p>
+        <h1>Here Are All My Apps!</h1>
+        <hr />
+        <BonjourApp />
+        <hr />
+        <MyStateApp />
       </header>
     </div>
   );
 }
 
-//
-// Second App: demonstrates how to use multiple instances of the same state
-//
+// BonjourApp: says Hello World in French
+function BonjourApp() {
+  return (
+    <h1>Bonjour, monde!</h1>
+  );
+}
+
+// MyStateApp: demonstrates how to use multiple instances of the same state
 function MyStateApp() {
   return (
-    <div>
+    <div className="App">
       <h1>Counters that update separately</h1>
       <MyButton />
       <MyButton />
@@ -32,6 +37,7 @@ function MyStateApp() {
   );
 }
 
+// MyButton: button that counts how many times it's been clicked
 function MyButton() {
   const [count, setCount] = useState(0);
 
@@ -47,4 +53,5 @@ function MyButton() {
 }
 
 // export default BonjourApp;    // First App above
-export default MyStateApp;    // Second App above
+// export default MyStateApp;    // Second App above
+export default AllMyApps;    // All the Apps above
