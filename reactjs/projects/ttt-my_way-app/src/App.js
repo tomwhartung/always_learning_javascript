a6049f7b0766aaf5afb419bbbb90fe5589c62733
Tom W Hartung <tomwhartung@gmail.com>
2023-03-15 13:16:02 -0600

    Updated reactjs/projects/ttt-my_way-app/src/App.js as described in the substeps to Step 4 in reactjs/tutorials/3-quick_start/2a-tic_tac_toe-my_way.md .  Note that handleClick is hard-coded to update only the first square, so we are not done yet!

--- reactjs/projects/ttt-my_way-app/src/App.js
+++ reactjs/projects/ttt-my_way-app/src/App.js
@@ -3,10 +3,11 @@ import './styles.css';
 import { useState } from 'react';
 
 // Square: function component defining a square on the tic-tac-toe board
-function Square( {value} ) {
+function Square( {value, onSquareClick} ) {
   return (
     <button
       className="square"
+      onClick={onSquareClick}
     >
       {value}
     </button>
@@ -15,22 +16,29 @@ function Square( {value} ) {
 
 export default function Board() {
   const [squares, setSquares] = useState( Array(9).fill(null) );
+
+  function handleClick() {
+    const nextSquares = squares.slice();   // Creates a new copy of the squares array
+    nextSquares[0] = "X";                  // Updates the first one (!!!)
+    setSquares(nextSquares);               // Sets squares equal to the new copy
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
       </div>
     </>
   );
