ea84765aa5afc3544ac15c3e7dded227aa16fb42
Tom W Hartung <tomwhartung@gmail.com>
2023-03-25 17:26:41 -0600

    Updated reactjs/projects/ttt-their_way-app/src/App.js with code changes described in subsection "3.1.1. Lifting state up - Part 1: Moving the `squares` state variable from `Square` to `Board`" in reactjs/tutorials/3-quick_start/2b-tic_tac_toe-their_way.md .

--- reactjs/projects/ttt-their_way-app/src/App.js
+++ reactjs/projects/ttt-their_way-app/src/App.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 
-function Square() {
-  const [value, setValue] = useState(null);
+function Square( { value } ) {
 
   function handleClick() {
-    setValue('X');
+    // setValue('X');
   }
 
   return (
@@ -18,22 +17,24 @@ function Square() {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </div>
   );
