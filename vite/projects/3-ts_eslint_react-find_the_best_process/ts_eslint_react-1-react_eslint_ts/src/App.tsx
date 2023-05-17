326dcd4c7ca9fa9992aaed46e91e0ab1dc63c5ca
Tom W Hartung <tomwhartung@gmail.com>
2023-05-17 15:00:55 -0600

    Updates made to vite/projects/3-ts_eslint_react-find_the_best_process/ts_eslint_react-1-react_eslint_ts/src/App.tsx , changing "import"s to "require"s as described in vite/notes/3a-ts_eslint_react-1-react_eslint_ts.md .

--- vite/projects/3-ts_eslint_react-find_the_best_process/ts_eslint_react-1-react_eslint_ts/src/App.tsx
+++ vite/projects/3-ts_eslint_react-find_the_best_process/ts_eslint_react-1-react_eslint_ts/src/App.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+// import reactLogo from './assets/react.svg'
+const reactLogo = require("./assets/react.svg");
+
+// import viteLogo from '/vite.svg'
+const viteLogo = require("/vite.svg");
+
 import './App.css'
 
 function App() {
