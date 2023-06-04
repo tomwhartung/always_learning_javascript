d1843f08409325832563cedea6751b94b8e2bb87
Tom W Hartung <tomwhartung@gmail.com>
2023-06-04 15:54:19 -0600

    Added the MySlider function component to and replaced demo code in mdbootstrap/projects/6-get_multiple_MySliders_to_work/src/App.tsx with code to use it in the single card the App still contains.  This version is roughly equivalent to what we have in mdbootstrap/notes/5-try_to_get_slider_to_work and corresponds to the tasks listed in subsection "3.2.1. Step 1" in mdbootstrap/notes/6-get_multiple_MySliders_to_work.md .

--- mdbootstrap/projects/6-get_multiple_MySliders_to_work/src/App.tsx
+++ mdbootstrap/projects/6-get_multiple_MySliders_to_work/src/App.tsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import { MDBRange } from 'mdb-react-ui-kit';
+
+// MySlider: function component interface to the MDBRange component
+function MySlider() {
+  return (
+    <MDBRange
+      defaultValue={50}
+      id='customRange'
+      label='My First MDBRange Component'
+    />
+  );
+}
+
+// App: this App's "mainline" component
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>My Four <code>MySlider</code>s App</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <MySlider />
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Try using one MySlider in each card.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+      </div>
     </>
   )
 }
