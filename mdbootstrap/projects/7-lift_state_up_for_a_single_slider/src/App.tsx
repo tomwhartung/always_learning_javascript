1d98a361b1c902f398e32c9390fb4dc4b9100a30
Tom W Hartung <tomwhartung@gmail.com>
2023-06-12 15:58:52 -0600

    Working on getting mdbootstrap/projects/7-lift_state_up_for_a_single_slider/src/App.tsx to display just one slider.  This version kinda works but needs some fixing and then some cleanup.

--- mdbootstrap/projects/7-lift_state_up_for_a_single_slider/src/App.tsx
+++ mdbootstrap/projects/7-lift_state_up_for_a_single_slider/src/App.tsx
@@ -1,33 +1,160 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//
+// App.tsx: Main file for the 6-get_multiple_MySliders_to_work project
+// -------------------------------------------------------------------
+//
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { ChangeEvent, useState } from 'react';
+import { MDBRange } from 'mdb-react-ui-kit';
 
+// Important constants
+const defaultValue = 50;
+// const numberOfSliderCards = 4;      // Warning: Do not make this greater
+//                                     // than or equal to the number of
+//                                     // elements in ordinalsArray!
+const ordinalsArray: readonly string[] = [
+  // "Zeroeth",
+  "Only",
+  "First",
+  "Second",
+  "Third",
+  "Fourth",
+  "Fifth",
+  "Sixth",
+];
+
+// Important types
+interface MySliderCardProps {
+  sliderNo: number;
+  onSliderChange: (evt: ChangeEvent<Element>) => void;
+}
+
+// MySlider: function component interface to the MDBRange component
+function MySlider( props:MySliderCardProps ) {
+  const [value, setValue] = useState(0)
+  const sliderLabel = ordinalsArray[props.sliderNo + 1] + " MySlider Component";
+  const sliderId = "myslider-" + Number(props.sliderNo);
+
+  function handleChange(evt:ChangeEvent) {
+    const val = (evt.target as HTMLInputElement).value;
+    // console.log("Value of " + sliderId + " is now " + val);
+    setValue(Number(val));
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <MDBRange
+        defaultValue={defaultValue}
+        id={sliderId}
+        label={sliderLabel}
+        onChange={props.onSliderChange}
+      />
+      <p>sliderValue = {value}</p>
+    </>
+  );
+}
+
+// MySliderCard: function component interface to the MDBRange component
+function MySliderCard( props:MySliderCardProps ) {
+  const ordinal = ordinalsArray[props.sliderNo + 1];
+  // console.log( "props.sliderNo = " + props.sliderNo );
+  const lcOrd = ordinal.toLowerCase();
+
+  return (
+    <div className="card">
+      <MySlider sliderNo={props.sliderNo} onSliderChange={props.onSliderChange} />
+      <p>
+        "myslider-{props.sliderNo}" in the {lcOrd} card.
       </p>
+    </div>
+  )
+}
+
+// MyContainer: function component containing an MDB container
+function MyContainer() {
+  // const [value, setValue] = useState(defaultValue);
+  const slNo = 0;
+
+  // function handleSliderChange(evt: ChangeEvent) {
+  function logSliderChange(slNo: number) {
+    // const val = (evt.target as HTMLInputElement).value;
+    console.log("logSliderChange: slNo = " + slNo);
+    // setValue(Number(val));
+  }
+
+  //      onSliderChange={() => handleSliderChange( (this as MySlider).onChange )} />
+  return (
+    <div className="container">
+      <div className="row">
+        <div key={slNo} className="col-md-6">
+          <MySliderCard
+            sliderNo={slNo}
+            onSliderChange={() => logSliderChange(slNo)} />
+        </div>
+      </div>
+      <div className="row mt-4 d-flex justify-content-center">
+        <div className="col-md-8">
+          <div className="card">
+            <p>TODO: Summarize the results here</p>
+            <p>This is example 1 of how to center 8 column units</p>
+            <p>
+             This is an example of a paragraph with a lot of content.
+             We want it to wrap around to a new line.
+             Pardon the bs.
+             Just adding filler.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="row mt-4">
+        <div className="col-md-2">
+        </div>
+        <div className="col-md-8">
+          <div className="card">
+            <p>TODO: Summarize the results here</p>
+            <p>This is example 2 of how to center 8 column units</p>
+          </div>
+        </div>
+      </div>
+      <div className="row mt-4 d-flex justify-content-center">
+        <div className="col-md-2">
+        </div>
+        <div className="col-md-8">
+          <div className="card">
+            <p>TODO: Summarize the results here</p>
+            <p>This is example 3 of how to center 8 cols between 2+2=4</p>
+            <p>
+             This is an example of a paragraph with a lot of content.
+             We want it to wrap around to a new line.
+             Pardon the bs.
+             Just adding filler.
+            </p>
+          </div>
+        </div>
+        <div className="col-md-2">
+        </div>
+      </div>
+      <div className="row mt-4 d-flex justify-content-center">
+        <div className="col-md-2">
+        </div>
+        <div className="col-md-8">
+          <div className="card">
+            <p>TODO: Summarize the results here</p>
+            <p>This is example 4 of how to center 8 cols between 2+2=4</p>
+          </div>
+        </div>
+        <div className="col-md-2">
+        </div>
+      </div>
+    </div>
+  )
+}
+
+// App: this App's "mainline" component
+function App() {
+  return (
+    <>
+      <h1>My Four <code>MySlider</code>s App</h1>
+      <MyContainer />
     </>
   )
 }
