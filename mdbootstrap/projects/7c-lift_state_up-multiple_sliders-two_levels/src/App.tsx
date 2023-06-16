8ef0c5edde8850e669dcde9bf1d4ddb64e435883
Tom W Hartung <tomwhartung@gmail.com>
2023-06-16 17:37:33 -0600

    Figured out how to use two values in sliders defined in a list but using the array of numbers in mdbootstrap/projects/7c-lift_state_up-multiple_sliders-two_levels/src/App.tsx .

--- mdbootstrap/projects/7c-lift_state_up-multiple_sliders-two_levels/src/App.tsx
+++ mdbootstrap/projects/7c-lift_state_up-multiple_sliders-two_levels/src/App.tsx
@@ -81,19 +81,22 @@ function MyContainer() {
   // const [value, setValue] = useState(Array(numberOfSliderCards).fill(defaultValue));
   const [value1, setValue1] = useState(defaultValue);
   const [value2, setValue2] = useState(defaultValue);
+  const slNo1 = 0;
+  const slNo2 = 1;
 
 // const [values, setValues] = useState<number[]>([]);
   const [values, setValues] = useState([defaultValue]);
   const [sliderValues, setSliderValues] = useState<SliderValue[]>([]);
+// const sliderNumberCols = [];
+// const sliderObjectCols = [];
 
 // Huh.  This causes a "Too many re-renders" error in the JS Console.  Hmmm-K.  Good to know!
 // for ( let slNo = 0; slNo < numberOfSliderCards; slNo++ ) {
 //   setValues(previousValues => [...previousValues, defaultValue]);
 //   setSliderValues(previousSliderValues => [...previousSliderValues, defaultSliderValue]);
 // }
-  const slNo1 = 0;
-  const slNo2 = 1;
 
+// function handleChange1(evt:ChangeEvent) {
   function handleChange1(evt:ChangeEvent) {
     const val = (evt.target as HTMLInputElement).value;
   // console.log("Value of this slider is now " + val);
@@ -105,30 +108,82 @@ function MyContainer() {
     setValue2(Number(val));
   }
 
+  function handleChangeNumber(evt:ChangeEvent, col:number) {
+    const val = (evt.target as HTMLInputElement).value;
+  // console.log("Value of this slider is now " + val);
+  // setValues(Number(val));
+    const nextValues = values.slice();
+    nextValues[col] = Number(val);
+    setValues(nextValues);
+  }
+  function handleChangeObject(evt:ChangeEvent) {
+    const val = (evt.target as HTMLInputElement).value;
+  // console.log("Value of this slider is now " + val);
+  // setSliderValues(Number(val));
+  const nextSliderValues = sliderValues.slice();
+  nextSliderValues[0] = { slNo: 0, value: Number(val) };
+  setSliderValues(nextSliderValues);
+}
+
+// for ( let col = 0; col < numberOfSliderCards; col++ ) {
+//   sliderNumberCols.push(
+//     <div key={col} className="col-md-3">
+//       <MySliderCard
+//        sliderNo={col}
+//        sliderVal={values[col]}
+//        onSliderChange={ () => handleChangeNumber(col) }
+//       />
+//     </div>
+//   );
+// }
+
+// for ( let col = 0; col < numberOfSliderCards; col++ ) {
+//   sliderObjectCols.push(
+//     <div key={col} className="col-md-3">
+//       <MySliderCard
+//        sliderNo={col}
+//        sliderVal={sliderValues[col].value}
+//        onSliderChange={ () => handleChangeObject(col) }
+//       />
+//     </div>
+//   );
+// }
+
+  // {sliderNumberCols}
+  // {sliderObjectCols}
   return (
     <div className="container">
       <div className="row d-flex justify-content-center">
-        <div key={slNo1} className="col-md-3">
+        <h3>Array of Numbers</h3>
+        <div key={0} className="col-md-3">
           <MySliderCard
-            sliderNo={slNo1}
-            sliderVal={value1}
-            onSliderChange={handleChange1}
+           sliderNo={0}
+           sliderVal={values[0]}
+           onSliderChange={ (evt) => handleChangeNumber(evt,0) }
           />
         </div>
-        <div key={slNo2} className="col-md-3">
+        <div key={1} className="col-md-3">
           <MySliderCard
-            sliderNo={slNo2}
-            sliderVal={value2}
-            onSliderChange={handleChange2}
+           sliderNo={1}
+           sliderVal={values[1]}
+           onSliderChange={ (evt) => handleChangeNumber(evt,1) }
           />
         </div>
       </div>
       <div className="row d-flex justify-content-center">
+        <h3>Array of SliderValue Objects</h3>
+        <div className="col-md-3">
+        </div>
+        <div className="col-md-3">
+        </div>
+      </div>
+      <div className="row d-flex justify-content-center">
+        <h3>Hard-coded List - Not an Array</h3>
         <div className="col-md-3">
           <MySliderCard
             sliderNo={slNo1}
             sliderVal={value1}
-            onSliderChange={handleChange1}
+            onSliderChange={() => handleChange1}
           />
         </div>
         <div className="col-md-3">
