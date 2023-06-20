806fca17da6f0a5c42fa06c95f7df2dec4c1a879
Tom W Hartung <tomwhartung@gmail.com>
2023-06-20 13:21:54 -0600

    Getting started on organizing mdbootstrap/projects/8-test_css_groja_grid/src/App.tsx the way I like, with a MyContainer component that references other components that contain the main content.  Keeping the old functionality, that counts the times the user clicks a button, around in a YeOldeCard component for the time being, as a sort of sanity check, until I get state working in the MyGridOfSquares component, which is where I will focus my efforts, at least initially.

--- mdbootstrap/projects/8-test_css_groja_grid/src/App.tsx
+++ mdbootstrap/projects/8-test_css_groja_grid/src/App.tsx
@@ -1,28 +1,59 @@
+//
+// src/App.tsx: Main file for the 8-test_css_groja_grid project
+// ------------------------------------------------------------
+//
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+// MyGridOfSquares: the grid of responsive squares that are the main feature of this project
+function MyGridOfSquares () {
+  return (
+    <div className="row mt-3">
+      <div className="col-12">
+        <div className="card">
+          <p>MyGridOfSquares goes here</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+// YeOldeCard: The original card I want to save temporarily "for posterity"
+function YeOldeCard () {
   const [count, setCount] = useState(0)
 
   return (
+    <div className="row mt-3">
+      <div className="col-12">
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Version 1: Click on the button to change the color in the square
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+// MyContainer: Main container holding most of the page's content
+function MyContainer() {
+  return (
     <>
-      <div>
-        <p>Sorry, but I thought the logos were crufty so I deleted them!</p>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MyGridOfSquares />
+      <YeOldeCard />
     </>
   )
 }
 
-export default App
+// App: this App's "mainline" component
+export default function App() {
+  return (
+    <>
+      <h3>Welcome to the <code className="bg-black text-white">8-test_css_groja_grid</code> project!</h3>
+      <MyContainer />
+    </>
+  )
+}
+
