3a2ce198e439b81264e97111e89b16b798ef6dfb
Tom W Hartung <tomwhartung@gmail.com>
2023-06-20 14:20:40 -0600

    Added new ResponsiveSquare component to mdbootstrap/projects/8-test_css_groja_grid/src/App.tsx .  So far, ResponsiveSquare is neither responsive nor a square, but I am working on it!  Also made some fairly cosmetic changes to the page.

--- mdbootstrap/projects/8-test_css_groja_grid/src/App.tsx
+++ mdbootstrap/projects/8-test_css_groja_grid/src/App.tsx
@@ -5,13 +5,36 @@
 import { useState } from 'react'
 import './App.css'
 
+// Important types
+
+// Important constants
+const gridSize = 3;   // number of rows and columns in the grid
+
+// ResponsiveSquare: A square in the grid that responds to clicks
+function ResponsiveSquare() {
+  return (
+    <div>
+      I am a ResponsiveSquare
+    </div>
+  )
+}
 // MyGridOfSquares: the grid of responsive squares that are the main feature of this project
 function MyGridOfSquares () {
+  const gridSquares = [];
+  for( let row=0; row < gridSize; row++ ) {
+    for( let col=0; col < gridSize; col++ ) {
+      gridSquares.push(
+        <ResponsiveSquare />
+      )
+    }
+  }
+
   return (
     <div className="row mt-3">
       <div className="col-12">
+        <h4 className='mt-3'><code>MyGridOfSquares</code></h4>
         <div className="card">
-          <p>MyGridOfSquares goes here</p>
+        {gridSquares}
         </div>
       </div>
     </div>
@@ -24,12 +47,14 @@ function YeOldeCard () {
   return (
     <div className="row mt-3">
       <div className="col-12">
+      <h4 className='mt-3'><code>YeOldeCard</code></h4>
         <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
           </button>
-          <p>
-            Version 1: Click on the button to change the color in the square
+          <h6 className='mt-4'>State handling sanity check:</h6>
+          <p className='mt-2'>
+            Click on the button to add to the count = {count}
           </p>
         </div>
       </div>
@@ -42,6 +67,8 @@ function MyContainer() {
   return (
     <>
       <MyGridOfSquares />
+      <hr className='hr mt-3' />
+      <hr className='hr mt-3' />
       <YeOldeCard />
     </>
   )
@@ -51,7 +78,9 @@ function MyContainer() {
 export default function App() {
   return (
     <>
-      <h3>Welcome to the <code className="bg-black text-white">8-test_css_groja_grid</code> project!</h3>
+      <h2>
+        Welcome to the <code className="bg-black text-white">8-test_css_groja_grid</code> project!
+      </h2>
       <MyContainer />
     </>
   )
