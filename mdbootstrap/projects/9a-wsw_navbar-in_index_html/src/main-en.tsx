dea4bb38254043ebefccd9d8daa9d5a92084dbd0
Tom W Hartung <tomwhartung@gmail.com>
2023-07-20 14:17:35 -0600

    Adding mdbootstrap/projects/9a-wsw_navbar-in_index_html/src/main-en.tsx and mdbootstrap/projects/9a-wsw_navbar-in_index_html/src/AppEn.tsx , trivial pages that ultimately display "Hello, world".

---
+++ mdbootstrap/projects/9a-wsw_navbar-in_index_html/src/main-en.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AppEn from './AppEn.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AppEn />
+  </React.StrictMode>,
+)
