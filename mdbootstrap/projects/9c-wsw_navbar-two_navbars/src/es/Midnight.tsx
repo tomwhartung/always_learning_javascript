9602a695fb30fed68c756f4b0a40468623706d3f
Tom W Hartung <tomwhartung@gmail.com>
2023-07-22 13:35:06 -0600

    Adding directories mdbootstrap/projects/9c-wsw_navbar-two_navbars/src/en/ , mdbootstrap/projects/9c-wsw_navbar-two_navbars/src/es/ , and mdbootstrap/projects/9c-wsw_navbar-two_navbars/src/fr/ , which contain trivial components used for testing the second menu.

---
+++ mdbootstrap/projects/9c-wsw_navbar-two_navbars/src/es/Midnight.tsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function Midnight() {
+  return (
+    <div id="midnight">
+      <h1>¡Buenas noches!</h1>
+    </div>
+  )
+}
+
+export default Midnight
---
+++ mdbootstrap/projects/9c-wsw_navbar-two_navbars/src/fr/Pm.tsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function Pm() {
+  return (
+    <div id="pm">
+      <h1>Au revoir!</h1>
+    </div>
+  )
+}
+
+export default Pm
