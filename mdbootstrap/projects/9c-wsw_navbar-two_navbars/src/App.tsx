1eb37cf490bd17c1324d5362ca16ff92063de7af
Tom W Hartung <tomwhartung@gmail.com>
2023-07-22 10:45:39 -0600

    Adding project directory mdbootstrap/projects/9c-wsw_navbar-two_navbars/ .  For details, see the notes file mdbootstrap/notes/9c-wsw_navbar-two_navbars.md .

---
+++ mdbootstrap/projects/9c-wsw_navbar-two_navbars/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div id="app">
+      <h2>En - English: Home page</h2>
+      <h2>Es - Espagnol: Página de inicio</h2>
+      <h2>Fr - Français: Page d'accueil</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ mdbootstrap/projects/9c-wsw_navbar-two_navbars/src/AppEs.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function AppEs() {
+  return (
+    <div id="app">
+      <h2>Hola, Mundo</h2>
+    </div>
+  )
+}
+
+export default AppEs
