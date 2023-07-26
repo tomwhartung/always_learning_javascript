14233effafe734d6fbe2c7173792a134e8648210
Tom W Hartung <tomwhartung@gmail.com>
2023-07-26 14:43:38 -0600

    Copied the English language files in mdbootstrap/projects/9c-wsw_navbar-two_navbars/src/en/ to whole_shebang/projects/4-wsw_navbar/src/en .

---
+++ whole_shebang/projects/4-wsw_navbar/src/en/Noon.tsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function Noon() {
+  return (
+    <div id="noon">
+      <h1>It's lunchtime!</h1>
+    </div>
+  )
+}
+
+export default Noon
---
+++ whole_shebang/projects/4-wsw_navbar/src/en/Pm.tsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function Pm() {
+  return (
+    <div id="pm">
+      <h1>Goodbye!</h1>
+    </div>
+  )
+}
+
+export default Pm
