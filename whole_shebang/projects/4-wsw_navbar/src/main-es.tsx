f95bfdf70ab305114f232f14fec514ca1b429c1a
Tom W Hartung <tomwhartung@gmail.com>
2023-07-26 15:10:43 -0600

    Copied the mdbootstrap/projects/9c-wsw_navbar-two_navbars/src/main* files to whole_shebang/projects/4-wsw_navbar/src/ .

---
+++ whole_shebang/projects/4-wsw_navbar/src/main-es.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
+
+import AppEs from './AppEs.tsx'
+import Am from './es/Am.tsx'
+import Noon from './es/Noon.tsx'
+import Pm from './es/Pm.tsx'
+import Midnight from './es/Midnight.tsx'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <nav className="navbar fixed-top navbar-expand navbar-light bg-light">
+        <div className="container-fluid justify-content-center">
+          <ul className="navbar-nav mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link link-secondary" href="index.html">
+                <i className="fas fa-house"></i>
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="Am">AM</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="Noon">Noon</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="Pm">PM</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="Midnight">Midnight</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="container d-flex justify-content-center">
+        <Routes>
+          <Route path="/Am" element={<Am />} />
+          <Route path="/Noon" element={<Noon />} />
+          <Route path="/Pm" element={<Pm />} />
+          <Route path="/Midnight" element={<Midnight />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+    <AppEs />
+  </React.StrictMode>,
+)
