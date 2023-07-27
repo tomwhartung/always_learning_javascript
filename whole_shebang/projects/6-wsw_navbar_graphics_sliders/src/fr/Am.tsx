ee9732f946ba9c5a81077925ef46c8c0141deb0e
Tom W Hartung <tomwhartung@gmail.com>
2023-07-27 17:19:17 -0600

    Copied the French language files in whole_shebang/projects/4-wsw_navbar/src/fr/ to whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/fr .

---
+++ whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/fr/Am.tsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function Am() {
+  return (
+    <div id="am">
+      <h1>Bonjour!</h1>
+    </div>
+  )
+}
+
+export default Am
---
+++ whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/fr/Midnight.tsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function Midnight() {
+  return (
+    <div id="midnight">
+      <h1>Bonne nuit!</h1>
+    </div>
+  )
+}
+
+export default Midnight
