a4a087af84e5dd218b451903be6c8b1647a8ac77
Tom W Hartung <tomwhartung@gmail.com>
2023-07-30 13:54:50 -0600

    Added the code for a simple Canvas to whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/AppBig5.tsx , copied from the whole_shebang/projects/2-wsw_graphics project.

--- whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/AppBig5.tsx
+++ whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/AppBig5.tsx
@@ -1,9 +1,57 @@
 import './App.css'
 
+import Canvas from './lib/Canvas.tsx';
+
+const canvasWidth = 200;
+const canvasHeight = 200;
+
+// draw: Minimal draw function to ensure the Canvas works
+const draw = (context: CanvasRenderingContext2D) => {
+  const width = canvasWidth;
+  const height = canvasHeight;
+
+  // Paint it all black
+  context.fillStyle = "rgb(0, 0, 0)";
+  context.fillRect(0, 0, width, height);
+
+  // Paint 4-8 colored squares inside the image
+  context.fillStyle = "rgba(255, 0, 0, 100)";    // Red
+  context.fillRect(0, 150, 50, 50);
+  context.fillRect(0, 50, 50, 50);
+  context.fillStyle = "rgba(0, 255, 0, 100)";    // Green
+  context.fillRect(50, 100, 50, 50);
+  context.fillRect(50, 0, 50, 50);
+  context.fillStyle = "rgba(0, 0, 255, 100)";    // Blue
+  context.fillRect(100, 50, 50, 50);
+  context.fillRect(100, 150, 50, 50);
+  context.fillStyle = "rgba(255, 255, 0, 100)";  // Yellow
+  context.fillRect(150, 0, 50, 50);
+  context.fillRect(150, 100, 50, 50);
+};
+
+// AppBig5: the "mainline" component for this quiz type
 function AppBig5() {
+  const width = canvasWidth;
+  const height = canvasHeight;
+
+  function handleImageClick(event: React.MouseEvent<HTMLElement>) {
+    const rect = (event.target as HTMLElement).getBoundingClientRect();
+    const pixelX = Math.round( event.clientX - rect.left );
+    const pixelY = Math.round( event.clientY - rect.top );
+    console.log( "Click detected at (" + pixelX.toString() + ", " + pixelY.toString() + ")." );
+  }
+
   return (
     <div id="app-big5">
       <h2>Big 5: The OCEAN types</h2>
+      <div className="col">
+        <Canvas
+          draw={draw}
+          onClick={handleImageClick}
+          width={width}
+          height={height}
+        />
+      </div>
     </div>
   )
 }
