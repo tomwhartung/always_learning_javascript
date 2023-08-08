14b4dca21f4b11ce6b2373c55490538606034558
Tom W Hartung <tomwhartung@gmail.com>
2023-08-08 14:13:26 -0600

    Renamed the MySlider component in whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/jungian/SliderCard.tsx to JungianSlider and removed some crufty comments from the file.

--- whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/jungian/SliderCard.tsx
+++ whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/jungian/SliderCard.tsx
@@ -2,29 +2,12 @@
 // SliderCard.tsx: code for the SliderCard component
 //
 
-// import '../App.css'
-// 
-// import { ChangeEvent, useState } from 'react';
-// 
-
 import { MDBRange } from 'mdb-react-ui-kit';
 import * as SliderLib from '../lib/SliderLib.tsx';
 import * as JungianLib from '../lib/JungianLib.tsx';
 
-// 
-// // ************************************************************************************************
-// // globalProps: A TEMPORARY GLOBAL variable to be replaced by a Context whatever in a later Project
-// // ************************************************************************************************
-// const globalProps: JungianImagePercents = {
-//   opacityPercent: valueToPct( SliderLib.defaultSliderValue ),
-//   blueVsYellowPercent: valueToPct( SliderLib.defaultSliderValue ),
-//   greenVsRedPercent: valueToPct( SliderLib.defaultSliderValue ),
-//   bAndYVsGandRPercent: valueToPct( SliderLib.defaultSliderValue ),
-// }
-// 
-
-// MySlider: function component interface to the MDBRange component
-function MySlider( props:SliderLib.SliderProps ) {
+// JungianSlider: Jungian-specific wrapper for the MDBRange component
+function JungianSlider( props:SliderLib.SliderProps ) {
   const sliderOppositeValue = 100 - props.sliderVal;
   const sliderId = "myslider-" + props.sliderNo.toString();
   let sliderLabel = sliderOppositeValue.toString() + "% " +
@@ -53,11 +36,11 @@ function MySlider( props:SliderLib.SliderProps ) {
   );
 }
 
-// SliderCardySliderCard: function component interface to the MDBRange component
+// SliderCard: MDB card wrapping a quiz-specific wrapper of the MDBRange component
 function SliderCard( props:SliderLib.SliderProps ) {
   return (
     <div className="card">
-      <MySlider
+      <JungianSlider
         sliderNo={props.sliderNo}
         sliderVal={props.sliderVal}
         onSliderChange={props.onSliderChange}
