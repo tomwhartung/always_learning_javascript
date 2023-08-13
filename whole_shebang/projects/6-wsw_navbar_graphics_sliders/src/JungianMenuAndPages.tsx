b4fed288dfe9de7439961c6ed3111571fd039626
Tom W Hartung <tomwhartung@gmail.com>
2023-08-13 11:33:53 -0600

    This version of whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/JungianMenuAndPages.tsx demonstrates that my idea of getting the values for the sliders saved in localStorage in this file and passing them on to the Create and other pages just plain will not work.  I can see the values in Chrome's developer tools and in the useEffect call, but they somehow get reset to the default values before I can pass them on to other pages, which are actually sub-components.  Rats.

--- whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/JungianMenuAndPages.tsx
+++ whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/JungianMenuAndPages.tsx
@@ -15,44 +15,55 @@ import Help from './jungian/Help.tsx'
 import './index.css'
 import './customizations.css'
 
-// These are the values we save in localStorage:
-let savedSliderValues: JungianLib.JungianImageProps = {
-  opacityValue: SliderLib.defaultSliderValue,
-  blueVsYellowValue: SliderLib.defaultSliderValue,
-  greenVsRedValue: SliderLib.defaultSliderValue,
-  bAndYVsGandRValue: SliderLib.defaultSliderValue,
-};
 let imageString: string[] = [];
-
 
 // JungianMenuAndPages: Jungian App's menu and pages
 function JungianMenuAndPages() {
+  let useEffectSliderValues: JungianLib.JungianImageProps = {
+    opacityValue: SliderLib.defaultSliderValue,
+    blueVsYellowValue: SliderLib.defaultSliderValue,
+    greenVsRedValue: SliderLib.defaultSliderValue,
+    bAndYVsGandRValue: SliderLib.defaultSliderValue,
+  };
   // If localStorage already has savedSliderValues and an imageString
   //   We want to use those values!
   useEffect(() => {
     const rawStoredSliderValues = localStorage.getItem( 'sliderValues' );
     if ( rawStoredSliderValues ) {
       const storedSliderValues = JSON.parse( rawStoredSliderValues );
-      savedSliderValues.opacityValue = storedSliderValues[0];
-      savedSliderValues.blueVsYellowValue = storedSliderValues[1];
-      savedSliderValues.greenVsRedValue = storedSliderValues[2];
-      savedSliderValues.bAndYVsGandRValue = storedSliderValues[3];
-      console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: savedSliderValues.opacityValue = " + savedSliderValues.opacityValue );
-      console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: savedSliderValues.blueVsYellowValue = " + savedSliderValues.blueVsYellowValue );
-      console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: savedSliderValues.greenVsRedValue = " + savedSliderValues.greenVsRedValue );
-      console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: savedSliderValues.bAndYVsGandRValue = " + savedSliderValues.bAndYVsGandRValue );
+      useEffectSliderValues.opacityValue = storedSliderValues[0];
+      useEffectSliderValues.blueVsYellowValue = storedSliderValues[1];
+      useEffectSliderValues.greenVsRedValue = storedSliderValues[2];
+      useEffectSliderValues.bAndYVsGandRValue = storedSliderValues[3];
+      console.log( "JungianMenuAndPages().useEffect: useEffectSliderValues.opacityValue = " + useEffectSliderValues.opacityValue );
+      console.log( "JungianMenuAndPages().useEffect: useEffectSliderValues.blueVsYellowValue = " + useEffectSliderValues.blueVsYellowValue );
+      console.log( "JungianMenuAndPages().useEffect: useEffectSliderValues.greenVsRedValue = " + useEffectSliderValues.greenVsRedValue );
+      console.log( "JungianMenuAndPages().useEffect: useEffectSliderValues.bAndYVsGandRValue = " + useEffectSliderValues.bAndYVsGandRValue );
     } else {
        console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: sliderValues NOT FOUND in localStorage!!!" );
     }
-    const rawStoredImageString = localStorage.getItem( 'imageString' );
-    if ( rawStoredImageString ) {
-      imageString = JSON.parse( rawStoredImageString );
-      console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: imageString = '" + imageString + "'" );
-    } else {
-       console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: imageString NOT FOUND in localStorage!!!" );
-    }
+  //const rawStoredImageString = localStorage.getItem( 'imageString' );
+  //if ( rawStoredImageString ) {
+  //  imageString = JSON.parse( rawStoredImageString );
+  //  console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: rawStoredImageString = '" + rawStoredImageString + "'" );
+  //  imageString = rawStoredImageString;
+  //  console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: imageString = '" + imageString + "'" );
+  //} else {
+  //   console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: imageString NOT FOUND in localStorage!!!" );
+  //}
   }, []);
+  // These are the values we save in localStorage:
+  let savedSliderValues: JungianLib.JungianImageProps = {
+    opacityValue: useEffectSliderValues.blueVsYellowValue,
+    blueVsYellowValue: useEffectSliderValues.blueVsYellowValue,
+    greenVsRedValue: useEffectSliderValues.greenVsRedValue,
+    bAndYVsGandRValue: useEffectSliderValues.bAndYVsGandRValue,
+  };
 
+  console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: savedSliderValues.opacityValue = " + savedSliderValues.opacityValue );
+  console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: savedSliderValues.blueVsYellowValue = " + savedSliderValues.blueVsYellowValue );
+  console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: savedSliderValues.greenVsRedValue = " + savedSliderValues.greenVsRedValue );
+  console.log( "JungianMenuAndPages() in JungianMenuAndPages.tsx: savedSliderValues.bAndYVsGandRValue = " + savedSliderValues.bAndYVsGandRValue );
 
   return (
     <BrowserRouter>
@@ -81,8 +92,25 @@ function JungianMenuAndPages() {
       </nav>
       <div className="container-fluid">
         <div className="row top-row">
+          <p>useEffectSliderValues.opacityValue = {useEffectSliderValues.opacityValue} and
+             savedSliderValues.opacityValue = {savedSliderValues.opacityValue}</p>
+          <p>useEffectSliderValues.blueVsYellowValue = {useEffectSliderValues.blueVsYellowValue} and
+             savedSliderValues.blueVsYellowValue = {savedSliderValues.blueVsYellowValue}</p>
+          <p>useEffectSliderValues.greenVsRedValue = {useEffectSliderValues.greenVsRedValue} and
+             savedSliderValues.greenVsRedValue = {savedSliderValues.greenVsRedValue}</p>
+          <p>useEffectSliderValues.bAndYVsGandRValue = {useEffectSliderValues.bAndYVsGandRValue} and
+             savedSliderValues.bAndYVsGandRValue = {savedSliderValues.bAndYVsGandRValue}</p>
+        </div>
+        <div className="row top-row">
           <Routes>
-            <Route path="/Create" element={<Create  />} />
+            <Route path="/Create" element=
+              {<Create
+                opacityValue={savedSliderValues.opacityValue}
+                blueVsYellowValue={savedSliderValues.blueVsYellowValue}
+                greenVsRedValue={savedSliderValues.greenVsRedValue}
+                bAndYVsGandRValue={savedSliderValues.bAndYVsGandRValue}
+              />}
+            />
             <Route path="/View" element={<View />} />
             <Route path="/Refine" element={<Refine />} />
             <Route path="/Help" element={<Help />} />
