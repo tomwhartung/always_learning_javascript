cf95999ea7385cf2b4344cee56a2f0397cb7210f
Tom W Hartung <tomwhartung@gmail.com>
2023-08-22 15:24:28 -0600

    Added the setImageString function to whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/lib/JungianLocalStorageLib.tsx .

--- whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/lib/JungianLocalStorageLib.tsx
+++ whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/lib/JungianLocalStorageLib.tsx
@@ -3,7 +3,33 @@
 //
 import * as JungianLib from '../lib/JungianLib.tsx';
 
-export const getImageString = ():string  => {
+// setImageString: sets the current imageString to the specified value
+//   returns true if successful else false if the newImageString is too short
+export function setImageString( newImageString: string ) {
+  if ( JungianLib.logLogicFlow ) {
+    console.log( "Top of setImageString() in JungianLocalStorageLib.tsx" );
+  }
+
+  let success = true;
+
+  if ( newImageString.length > JungianLib.gridSize ) {
+    console.log( "setImageString(): storing '" + newImageString + "' as imageString" );
+    localStorage.setItem( 'imageString', JSON.stringify(newImageString) );
+  } else {
+    console.log( "setImageString(): newImageString.length = " + newImageString.length + "' is imageString" );
+    console.log( "setImageString(): NOT saving newImageString because it is TOO SHORT" );
+    success = false;
+  }
+
+  if ( JungianLib.logLogicFlow ) {
+    console.log( "Return()ing from setImageString() in JungianLocalStorageLib.tsx" );
+  }
+
+  return success;
+}
+
+// getImageString: returns the current imageString from local storage
+export function getImageString():string {
   if ( JungianLib.logLogicFlow ) {
     console.log( "Top of getImageString() in JungianLocalStorageLib.tsx" );
   }
@@ -20,5 +46,5 @@ export const getImageString = ():string  => {
   }
 
   return imageString;
-};
+}
 
