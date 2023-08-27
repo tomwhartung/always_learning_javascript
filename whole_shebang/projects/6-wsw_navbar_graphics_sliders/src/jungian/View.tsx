c900bd787733c69d7e19d934e80376590acc5c2c
Tom W Hartung <tomwhartung@gmail.com>
2023-08-27 15:24:11 -0600

    Renamed the DFlexViewSliderValuesAndImage component to DFlexImageAndSliderValues and removed the onClick attribute from the Canvas element in whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/jungian/View.tsx .  Also refined some and deleted some other commented-out console.log statements, and added "Please" to the createOrRefineMessage .

--- whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/jungian/View.tsx
+++ whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/jungian/View.tsx
@@ -10,8 +10,8 @@ import * as JungianLib from '../lib/JungianLib.tsx';
 import * as JungianLSLib from '../lib/JungianLocalStorageLib.tsx';
 
 // NOTE: Setting logLogicFlow to true for one page in effect sets it for all pages
-// JungianLib.setLogLogicFlow( true );     // un-comment when trying to track down issues
-JungianLib.setLogLogicFlow( false );    // un-comment when everything's ok
+// JungianLib.setLogLogicFlow( false );    // un-comment when everything's ok
+JungianLib.setLogLogicFlow( true );     // un-comment when trying to track down issues
 
 let storedImageString = "";
 let opacityValue = defaultSliderValue;
@@ -21,43 +21,31 @@ const draw = (context: CanvasRenderingContext2D) => {
   JungianLib.drawStoredImageString( context, storedImageString, opacityValue );
 };
 
-// DFlexViewSliderValuesAndImage: function component to display a jungian image
-function DFlexViewSliderValuesAndImage( props:JungianLib.JungianSliderValues ) {
+// DFlexImageAndSliderValues: function component to display a jungian image
+function DFlexImageAndSliderValues( props:JungianLib.JungianSliderValues ) {
   if ( JungianLib.logLogicFlow ) {
-    console.log( "Top of DFlexViewSliderValuesAndImage() in View.tsx" );
+    console.log( "Top of DFlexImageAndSliderValues() in View.tsx" );
   }
 
   const width = JungianLib.canvasWidth;
   const height = JungianLib.canvasHeight;
 
-  function handleImageClick(event: React.MouseEvent<HTMLElement>) {
-    const rect = (event.target as HTMLElement).getBoundingClientRect();
-    const pixelX = Math.round( event.clientX - rect.left );
-    const pixelY = Math.round( event.clientY - rect.top );
-    console.log( "Click on resizable image at (" + pixelX.toString() + ", " + pixelY.toString() + ")" );
-  }
-
-  // console.log( "DFlexViewSliderValuesAndImage() in View.tsx: props.opacityValue = " + props.opacityValue );
-  // console.log( "DFlexViewSliderValuesAndImage() in View.tsx: props.blueVsYellowValue = " + props.blueVsYellowValue );
-  // console.log( "DFlexViewSliderValuesAndImage() in View.tsx: props.greenVsRedValue = " + props.greenVsRedValue );
-  // console.log( "DFlexViewSliderValuesAndImage() in View.tsx: props.bAndYVsGandRValue = " + props.bAndYVsGandRValue );
-
   opacityValue = props.opacityValue;
 
   if ( JungianLib.logLogicFlow ) {
-    console.log( "Return()ing from DFlexViewSliderValuesAndImage() in View.tsx" );
+    console.log( "Return()ing from DFlexImageAndSliderValues() in View.tsx" );
   }
 
   return (
     <>
       <div className="row mt-4 d-flex justify-content-center">
-        <div className="col-md-8">
+        <div className="col-md-12">
           <div className="card jungian-canvas">
             <Canvas
               draw={draw}
-              onClick={handleImageClick}
               width={width}
-              height={height} />
+              height={height}
+            />
           </div>
         </div>
       </div>
@@ -93,25 +81,20 @@ function DFlexContainer() {
     }
     const storedSliderValues = JungianLSLib.getSliderValues();
     setCurrentSliderValues( storedSliderValues );
-    // console.log( "useEffect in DFlexContainer() in View.tsx: storedSliderValues[0] = " + storedSliderValues[0] );
-    // console.log( "useEffect in DFlexContainer() in View.tsx: storedSliderValues[1] = " + storedSliderValues[1] );
-    // console.log( "useEffect in DFlexContainer() in View.tsx: storedSliderValues[2] = " + storedSliderValues[2] );
-    // console.log( "useEffect in DFlexContainer() in View.tsx: storedSliderValues[3] = " + storedSliderValues[3] );
     storedImageString = JungianLSLib.getImageString();
     if ( JungianLib.logLogicFlow ) {
-      console.log( "Exiting useEffect in DFlexContainer() in View.tsx: storedImageString = '" + storedImageString + "'" );
+      // console.log( "useEffect in DFlexContainer: storedSliderValues[0] = " + storedSliderValues[0] );
+      // console.log( "useEffect in DFlexContainer: storedSliderValues[1] = " + storedSliderValues[1] );
+      // console.log( "useEffect in DFlexContainer: storedSliderValues[2] = " + storedSliderValues[2] );
+      // console.log( "useEffect in DFlexContainer: storedSliderValues[3] = " + storedSliderValues[3] );
+      console.log( "Exiting useEffect in DFlexContainer() in View.tsx" );
     }
   }, []);
-
-  // console.log( "DFlexContainer() in View.tsx: currentSliderValues[0] = " + currentSliderValues[0] );
-  // console.log( "DFlexContainer() in View.tsx: currentSliderValues[1] = " + currentSliderValues[1] );
-  // console.log( "DFlexContainer() in View.tsx: currentSliderValues[2] = " + currentSliderValues[2] );
-  // console.log( "DFlexContainer() in View.tsx: currentSliderValues[3] = " + currentSliderValues[3] );
 
   let createOrRefineMessage = "";
 
   if ( storedImageString.length === 0 ) {
-    createOrRefineMessage = "You must use the Create option to create an image before you can View it.";
+    createOrRefineMessage = "Please use the Create option to create an image before you can View it.";
   } else {
     createOrRefineMessage = "You can now use the Refine option to refine your image.";
   }
@@ -124,7 +107,7 @@ function DFlexContainer() {
     <div className="container">
       <div className="row mt-2 d-flex justify-content-center">
         <h5>{createOrRefineMessage}</h5>
-        <DFlexViewSliderValuesAndImage
+        <DFlexImageAndSliderValues
           opacityValue={currentSliderValues[0] ?? defaultSliderValue}
           blueVsYellowValue={currentSliderValues[1] ?? defaultSliderValue}
           greenVsRedValue={currentSliderValues[2] ?? defaultSliderValue}
