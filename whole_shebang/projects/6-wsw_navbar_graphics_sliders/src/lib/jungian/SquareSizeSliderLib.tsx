0ca69765ba2c5bcc4a641bfb133bf0928cafda28
Tom W Hartung <tomwhartung@gmail.com>
2023-09-13 15:17:38 -0600

    Renamed SquareSizeSliderProps to SquareSizeSliderPropsIFace in whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/lib/jungian/SquareSizeSliderLib.tsx .  Also, it turns out there is no need to export this interface so I made it "private".

--- whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/lib/jungian/SquareSizeSliderLib.tsx
+++ whole_shebang/projects/6-wsw_navbar_graphics_sliders/src/lib/jungian/SquareSizeSliderLib.tsx
@@ -1,19 +1,20 @@
 // 
 // lib/jungian/SquareSizeSliderLib.tsx: types and components used by the Jungian square size slider
+// ------------------------------------------------------------------------------------------------
 //
 import { ChangeEvent } from 'react';
 import { MDBRange } from 'mdb-react-ui-kit';
 
 import * as ImageLib from './ImageLib.ts';
 
-// SquareSizeSliderProps: props passed to SquareSizeSlider
-export interface SquareSizeSliderProps {
+// SquareSizeSliderPropsIFace: props passed to SquareSizeSlider
+interface SquareSizeSliderPropsIFace {
   squareSizeLabel: string;
   onSquareSizeChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
 // SquareSizeSlider: MDB card wrapping a quiz-specific wrapper of the MDBRange component
-export function SquareSizeSlider( props: SquareSizeSliderProps ) {
+export function SquareSizeSlider( props: SquareSizeSliderPropsIFace ) {
   return (
     <MDBRange
       defaultValue={ImageLib.squareSize}
